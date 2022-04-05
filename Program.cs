9b1a3420e5f3586192f8c6456a3ec304968a2091
GilsonRavaiani <gravaiani@yahoo.com.br>
2022-04-05 10:53:41 -0300

    ConversaoMaiuscula

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConversaoMaiuscula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+           var texto = "Este texto é um aMiGo teste";
+            Console.WriteLine(texto.ToLower());//Converte texto para minusculo
+            Console.WriteLine(texto.ToUpper());//Converte texto para MAIUSCULO
+            Console.WriteLine(texto.Insert(5, "AQUI"));//Inserir um Caracter dentro do texto
+            Console.WriteLine(texto.Remove(5, 5));//Remnove um Caracter dentro do texto
+            Console.WriteLine(texto.Length);//Informa a Quantidade de caracteres que possui a String
+            
+           
+        }
+    }
+}
